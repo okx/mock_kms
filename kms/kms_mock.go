adfb79729595629fada11d2db871366a3f7f24ca
xrqin <qxrmath@gmail.com>
2025-02-25 15:07:28 +0800

    move interface

--- kms/kms_mock.go
+++ kms/kms_mock.go
@@ -1,37 +1,18 @@
 package kms
 
-import (
-	"log"
-)
-
-// Global variables, similar to the real library
-var (
-	secretData = make(map[string]string) // Store mock "secret data"
-)
-
 // Init simulates initializing KMS client
 func Init() error {
 	return nil
 }
 
 // GetAwsSecretValue simulates getting a secret value
-func GetAwsSecretValue(key string) (string, error) {
-	log.Printf("[mock-kms] GetAwsSecretValue called for key=%s", key)
-	// If exists, return corresponding value, otherwise return mock
-	if val, ok := secretData[key]; ok {
-		// Pretend this is already decrypted result (or you can keep it as ciphertext)
-		return val, nil
-	}
+func GetAwsSecretValue(key string) string {
 	// If not found, return a fixed string
-	return "mock-" + key, nil
+	return key
 }
 
 // GetAwsSecretData returns all currently saved mock secrets
-func GetAwsSecretData() map[string]string {
+func GetAwsSecretData() map[string]error {
 	// Copy or return directly
-	dataCopy := make(map[string]string)
-	for k, v := range secretData {
-		dataCopy[k] = v
-	}
-	return dataCopy
+	return nil
 }
